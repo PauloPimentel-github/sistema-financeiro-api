bd2800a2f76b1ba3a75cfe43b013d51b02f95769
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-09-08 19:10:18 -0300

    First commit, adicionando estrutura inicial do projeto

---
+++ src/main/java/com/phpimentel/sistemafinanceiroapi/SistemaFinanceiroApiApplication.java
@@ -0,0 +1,13 @@
+package com.phpimentel.sistemafinanceiroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFinanceiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFinanceiroApiApplication.class, args);
+	}
+
+}
