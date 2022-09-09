98dd71362bea9fce8026ddf3ccc0fa16f74152b6
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-09-09 20:42:04 -0300

    Adicionando modelo de dominio para despesas

--- src/main/java/com/phpimentel/sistemafinanceiroapi/models/ExpenseModel.java
+++ src/main/java/com/phpimentel/sistemafinanceiroapi/models/ExpenseModel.java
@@ -3,26 +3,23 @@ package com.phpimentel.sistemafinanceiroapi.models;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.phpimentel.sistemafinanceiroapi.dtos.CategoryExpenseDto;
+import com.phpimentel.sistemafinanceiroapi.dtos.ExpenseDto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.beans.BeanUtils;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.OffsetDateTime;
-import java.util.Set;
 import java.util.UUID;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
-@Table(name = "category_expenses")
-public class CategoryExpenseModel implements Serializable {
+@Table(name = "expenses")
+public class ExpenseModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -34,6 +31,10 @@ public class CategoryExpenseModel implements Serializable {
     @Column(name = "name", nullable = false, length = 50)
     private String name;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private CategoryExpenseModel categoryExpense;
+
     @CreationTimestamp
     @Column(name="creation_date", nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
@@ -44,14 +45,9 @@ public class CategoryExpenseModel implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private OffsetDateTime lastUpdateDate;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @OneToMany(mappedBy = "categoryExpense", fetch = FetchType.LAZY)
-    @Fetch(FetchMode.SUBSELECT)
-    private Set<ExpenseModel> expenses;
-
-    public CategoryExpenseDto convertToCustomerDto() {
-        var categoryExpenseDto = new CategoryExpenseDto();
-        BeanUtils.copyProperties(this, categoryExpenseDto);
-        return categoryExpenseDto;
+    public ExpenseDto convertToExpenseDto() {
+        var expenseDto = new ExpenseDto();
+        BeanUtils.copyProperties(this, expenseDto);
+        return expenseDto;
     }
 }
