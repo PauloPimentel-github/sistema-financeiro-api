bbe42744c269dad3c5b2fcc0ba9951f532f544d2
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-09-13 19:34:50 -0300

    Adicionando dto e modelo de domínio para frequências

---
+++ src/main/java/com/phpimentel/sistemafinanceiroapi/models/FrequencyModel.java
@@ -0,0 +1,51 @@
+package com.phpimentel.sistemafinanceiroapi.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.phpimentel.sistemafinanceiroapi.dtos.FrequencyDto;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.beans.BeanUtils;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "frequencies")
+public class FrequencyModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    @Column(name = "days", nullable = false)
+    private Integer days;
+
+    @CreationTimestamp
+    @Column(name="creation_date", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private OffsetDateTime creationDate;
+
+    @UpdateTimestamp
+    @Column(name="last_update_date", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private OffsetDateTime lastUpdateDate;
+
+    public FrequencyDto convertToFrequencyDto() {
+        var frequencyDto = new FrequencyDto();
+        BeanUtils.copyProperties(this, frequencyDto);
+        return frequencyDto;
+    }
+}
